8d0c3ff81116ab8e9fa526929375e58d089a1807
Jacob Ballor <000109530@student.baisd.net>
2017-11-07 09:24:05 -0800

    Last Programming Problem from Chapter 3

---
+++ cs_eoc-_3-13_property_tax/Program3_13/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Program3_13
+{
+    partial class frmPropertyTax
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPropertyValue = new System.Windows.Forms.Label();
+            this.txtPropertyValue = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPropertyValue
+            // 
+            this.lblPropertyValue.AutoSize = true;
+            this.lblPropertyValue.Location = new System.Drawing.Point(13, 13);
+            this.lblPropertyValue.Name = "lblPropertyValue";
+            this.lblPropertyValue.Size = new System.Drawing.Size(95, 13);
+            this.lblPropertyValue.TabIndex = 0;
+            this.lblPropertyValue.Text = "Property\'s Value: $";
+            // 
+            // txtPropertyValue
+            // 
+            this.txtPropertyValue.Location = new System.Drawing.Point(105, 9);
+            this.txtPropertyValue.Name = "txtPropertyValue";
+            this.txtPropertyValue.Size = new System.Drawing.Size(100, 20);
+            this.txtPropertyValue.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(211, 7);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(66, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutput.Location = new System.Drawing.Point(13, 36);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(264, 23);
+            this.lblOutput.TabIndex = 3;
+            // 
+            // frmPropertyTax
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 71);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtPropertyValue);
+            this.Controls.Add(this.lblPropertyValue);
+            this.Name = "frmPropertyTax";
+            this.Text = "Program3_13";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPropertyValue;
+        private System.Windows.Forms.TextBox txtPropertyValue;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ cs_eoc-_3-13_property_tax/Program3_13/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Program3_13
+{
+    public partial class frmPropertyTax : Form
+    {
+        public frmPropertyTax()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //What happens when the user clicks the "Calculate" button?
+            //Your output should read something like "Sales tax is (number)."
+            double homeValue;
+            //To hold home value. 
+            double propertyTax;
+            //To hold the property tax value.
+            propertyTax = (100 * .64);
+            double salesTax;
+            //To hold the said value.
+            homeValue = double.Parse(txtPropertyValue.Text);
+            salesTax = homeValue / propertyTax;
+            lblOutput.Text = salesTax.ToString();
+
+        }
+    }
+}
